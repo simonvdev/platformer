69b0a1a8dcc1a414ba32ad7b2da774d23a51b06e
simonv <12126739+simonvdev@users.noreply.github.com>
2025-05-22 01:09:06 +0200

    Basic audio setup even though its short sighted

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,13 +1,21 @@
 extends Area2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var collision_shape_2d = $CollisionShape2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_sprite_2d.play("default")
 	body_entered.connect(_on_body_entered)
+	audio_stream_player_2d.finished.connect(_on_audio_finished)
 	
 	
 func _on_body_entered(body):
 	Director.CoinCollected.emit()
+	audio_stream_player_2d.play()
+	collision_shape_2d.disabled = true
+	animated_sprite_2d.visible = false
+
+func _on_audio_finished():
 	queue_free()
