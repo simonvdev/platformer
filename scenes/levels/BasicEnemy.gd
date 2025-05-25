34234c1083d187c446df1c7e699432d3381dbfb2
simonvdev <12126739+simonvdev@users.noreply.github.com>
2025-05-26 01:59:00 +0200

    Basic enemy setup goomba like, still need to add squishing but damage can already be dealt as it dies in the void, it also deals a heart of damage to the player.

---
+++ scenes/levels/BasicEnemy.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 175.0
+var direction = -1
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var health_component: HealthComponent = $HealthComponent
+
+func _ready() -> void:
+	health_component.died.connect(_on_death)
+
+func _on_death():
+	queue_free()
+
+func _process(delta: float) -> void:
+	animated_sprite_2d.flip_h = true if direction == 1 else false
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if ray_cast_2d.is_colliding():
+		var dir = ray_cast_2d.get_collision_point() - ray_cast_2d.global_position
+		if dir.length() < 40 :
+			direction *= -1
+			ray_cast_2d.target_position.x *= -1	 
+
+	velocity.x = SPEED * direction
+
+	move_and_slide()
